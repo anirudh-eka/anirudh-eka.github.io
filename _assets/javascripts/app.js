8b7e6ccdddefdab79b8f03bc6b3666d427e65e09
Anirudh Dhullipalla <rudysback@gmail.com>
2014-02-13 14:38:13 -0500

    add minor parallax scroll

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,1 +1,17 @@
-alert("hello world!");
+//= require vendor/jquery
+
+$(document).ready(function(){
+    $('#banner').each(function(){
+        var $bgobj = $(this); // assigning the object
+        $window = $(window);
+        $window.scroll(function() {
+            var yPos = ($window.scrollTop() / 5); 
+             
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
