4f33ce96340abf34cf8eaec99aae03822d7ab82f
Anirudh Dhullipalla <rudysback@gmail.com>
2014-02-13 15:31:38 -0500

    make auto scroll, when blog header is clicked

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,17 +1,22 @@
 //= require vendor/jquery
 
 $(document).ready(function(){
+    $window = $(window);
+    
     $('#banner').each(function(){
-        var $bgobj = $(this); // assigning the object
-        $window = $(window);
-        $window.scroll(function() {
-            var yPos = ($window.scrollTop() / 3); 
-             
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
- 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
-    });    
+      var $bgobj = $(this); // assigning the object
+      $window.scroll(function() {
+          var yPos = ($window.scrollTop() / 3); 
+           
+          // Put together our final background position
+          var coords = '50% '+ yPos + 'px';
+
+          // Move the background
+          $bgobj.css({ backgroundPosition: coords });
+      }); 
+    });
+
+    $('#home-body h1').on('click', function() {
+      $('html').animate({scrollTop: $('#banner').height()}, 600);
+    });
 });
