acd07ff3704000101853f179525be98b650237c3
Anirudh Eka <rudysback@gmail.com>
2017-06-02 17:11:10 +0530

    create shell script that takes an image of standard Instagram dimensions and crops img then saves to assets; add mango appologies post  Build site at 2017-06-02 11:41:08 UTC

---
+++ _posts/2017-06-02-mango-apologies.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "Mango Apologies"
+tags: India PictureWorthAPoem
+categories: ["poetry & essays"]
+share: discuss
+image_file: mango_apologies.png
+text_excerpt: Another mango drops. Another half hearted apology for the Indian summer. We take what we can get.
+---
+{% img mango_apologies alt="mango_apologies" %}
+
+Another mango drops. Another half hearted apology for the Indian summer. We take what we can get.
---
+++ _site/image_resizer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# image_resizer
+
+if [ $# -lt  2 ]; then 
+  echo 1>&2 "$0: not enough arguments"
+  echo "First Arg is the the orginal image file"
+  echo "Second Arg is the name of the new image. It will be saved in ./_assets/images/ as a png (no need to add extension)"
+  exit 2
+fi
+
+echo Converting Image...
+
+convert $1 -gravity center -crop 1080x617+0+0 -resize 700x400 ./_assets/images/$2.png
+
+open ./_assets/images/$2.png
